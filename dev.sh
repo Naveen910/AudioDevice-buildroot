a20321c97ccf61270ca6bb97365ed501c4077711
Naveen910 <naveenchary9550@gmail.com>
2026-02-13 12:26:06 +0530

    modified:   Readme 	modified:   custom-f1c100s-buildroot/configs/my_defconfig 	new file:   custom-f1c100s-buildroot/package/Config.in 	modified:   dev.sh

--- dev.sh
+++ dev.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# 1. Build the image if it doesn't exist
+# 1. Build the image
 docker build -t audio-builder . > /dev/null
 
-# 2. Run Interactive Shell
-# --entrypoint /bin/bash: Overrides your entrypoint.sh script
-# -it: Gives you an interactive terminal
+# 2. Run Interactive Shell with Pre-loaded Config
 echo "Entering Development Environment..."
 docker run -it --rm \
     -v "$(pwd):/workspace" \
     -v "$(pwd)/build-output:/output" \
     --user $(id -u):$(id -g) \
     --entrypoint /bin/bash \
-    audio-builder
+    audio-builder -c "
+        cd buildroot && \
+        make BR2_EXTERNAL=../custom-f1c100s-buildroot my_defconfig && \
+        exec bash
+    "
