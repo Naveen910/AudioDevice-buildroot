78eeeb6d2fe590786457714addc8b6e7b1872a74
Naveen910 <naveenchary9550@gmail.com>
2026-02-15 14:54:40 +0530

    modified:   Dockerfile 	modified:   Readme 	modified:   build.sh 	modified:   custom-f1c100s-buildroot/board/linux_defconfig 	modified:   custom-f1c100s-buildroot/board/suniv-f1c100s-audio.dts 	new file:   custom-f1c100s-buildroot/board/suniv-f1c100s.dtsi

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM ubuntu:22.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 
-# Install Buildroot dependencies
 RUN apt-get update && apt-get install -y \
     build-essential \
     git \
@@ -17,18 +16,21 @@ RUN apt-get update && apt-get install -y \
     libncursesw5-dev \
     cpio \
     python3 \
+    python3-pip \
+    python3-dev \
+    swig \
     file \
     locales \
     ca-certificates \
     xz-utils \
     && rm -rf /var/lib/apt/lists/*
 
-# Set locale
+# Install pylibfdt Python bindings
+RUN pip3 install --no-cache-dir pylibfdt
+
+# Locale (optional)
 RUN locale-gen en_US.UTF-8
 ENV LANG=en_US.UTF-8
 
-# Working directory
 WORKDIR /build
-
-# Default command
 CMD ["/bin/bash"]
--- build.sh
+++ build.sh
@@ -3,38 +3,69 @@ set -e
 
 # ---- CONFIG ----
 BUILDROOT_VERSION=2023.02.9
-BUILDROOT_DIR=buildroot
-EXTERNAL_DIR=custom-f1c100s-buildroot
-OUTPUT_DIR=output
+BUILDROOT_DIR="buildroot"
+EXTERNAL_DIR="custom-f1c100s-buildroot"
+
+O="/tmp/br-output"
+DL="/tmp/br-dl"
+
+# ---- Sanity Checks ----
+if [ ! -d "$EXTERNAL_DIR" ]; then
+    echo "ERROR: External tree '$EXTERNAL_DIR' not found!"
+    exit 1
+fi
 
 # ---- Download Buildroot if not present ----
 if [ ! -d "$BUILDROOT_DIR" ]; then
-    echo "Downloading Buildroot..."
+    echo "Downloading Buildroot ${BUILDROOT_VERSION}..."
     wget https://buildroot.org/downloads/buildroot-${BUILDROOT_VERSION}.tar.gz
-    tar -xvf buildroot-${BUILDROOT_VERSION}.tar.gz
-    mv buildroot-${BUILDROOT_VERSION} ${BUILDROOT_DIR}
+    tar -xf buildroot-${BUILDROOT_VERSION}.tar.gz
+    mv buildroot-${BUILDROOT_VERSION} "$BUILDROOT_DIR"
 fi
 
-# ---- Buildroot commands ----
-mkdir -p /tmp/br-output /tmp/br-dl
-cd ${BUILDROOT_DIR}
+mkdir -p "$O" "$DL"
+cd "$BUILDROOT_DIR"
 
-make O=/tmp/br-output \
-     BR2_DL_DIR=/tmp/br-dl \
-     BR2_EXTERNAL=../custom-f1c100s-buildroot \
+# ---- Load defconfig ----
+make O="$O" \
+     BR2_DL_DIR="$DL" \
+     BR2_EXTERNAL="../${EXTERNAL_DIR}" \
      my_defconfig
 
-make O=/tmp/br-output \
-     BR2_EXTERNAL=../custom-f1c100s-buildroot \
-     menuconfig
+# ---- If user wants interactive config ----
+if [ "$1" = "menu" ]; then
 
-make O=/tmp/br-output \
-     BR2_EXTERNAL=../custom-f1c100s-buildroot \
-     linux-menuconfig
+    echo "Opening Buildroot menuconfig..."
+    make O="$O" \
+         BR2_DL_DIR="$DL" \
+         BR2_EXTERNAL="../${EXTERNAL_DIR}" \
+         menuconfig
 
-#make O=/tmp/br-output \
-#    BR2_EXTERNAL=../custom-f1c100s-buildroot
+    echo "Saving Buildroot defconfig..."
+    make O="$O" \
+         BR2_DL_DIR="$DL" \
+         BR2_EXTERNAL="../${EXTERNAL_DIR}" \
+         savedefconfig \
+         BR2_DEFCONFIG="../${EXTERNAL_DIR}/configs/my_defconfig"
 
-#cp -r /tmp/br-output/images ../output-images
+    echo "Opening Linux kernel menuconfig..."
+    make O="$O" \
+         BR2_DL_DIR="$DL" \
+         BR2_EXTERNAL="../${EXTERNAL_DIR}" \
+         linux-menuconfig
 
-echo "Build completed!"
+    echo "Saving Linux kernel defconfig..."
+    make O="$O" \
+         BR2_DL_DIR="$DL" \
+         BR2_EXTERNAL="../${EXTERNAL_DIR}" \
+         linux-update-defconfig
+fi
+
+# ---- Final build ----
+echo "Building Buildroot..."
+make O="$O" \
+     BR2_DL_DIR="$DL" \
+     BR2_EXTERNAL="../${EXTERNAL_DIR}"
+
+echo "Build completed successfully!"
+echo "Output images are in: $O/images"
